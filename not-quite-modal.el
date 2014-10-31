a8b81251db434a36daa616e8beefb797fac8d585
Wilfred Hughes <me@wilfred.me.uk>
2014-10-31 00:11:54 +0000

    Adding skeleton file.

---
+++ not-quite-modal.el
@@ -0,0 +1,35 @@
+;;; not-quite-modal.el --- Modal-inspired movement
+
+;; Copyright (C) 2014 Wilfred Hughes <me@wilfred.me.uk>
+;;
+;; Author: Wilfred Hughes <me@wilfred.me.uk>
+;; Created: 31 October 2014
+;; Version: 0.1
+
+;;; Commentary:
+;; 
+
+;;; License:
+
+;; This file is not part of GNU Emacs.
+;; However, it is distributed under the same license.
+
+;; GNU Emacs is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; GNU Emacs is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Code:
+
+(provide 'not-quite-modal)
+;;; not-quite-modal.el ends here
