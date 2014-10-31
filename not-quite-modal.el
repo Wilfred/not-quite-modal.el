9021f0634e1372767404a205c4c9a722aebc3fe9
Wilfred Hughes <me@wilfred.me.uk>
2014-10-31 00:26:53 +0000

    Motivation and planned feature set.

--- not-quite-modal.el
+++ not-quite-modal.el
@@ -7,7 +7,26 @@
 ;; Version: 0.1
 
 ;;; Commentary:
-;; 
+;; If you install keyfreq (and you should!), you will probably find
+;; that movement commands are your most used commands. This is
+;; certainly true for me.
+;;
+;; However, if you're just *reading* code, it's a shame to type a
+;; modifier key every time. Modal editing solves this problem.
+;;
+;; Many Emacs users (myself included) have little vim expertise, so
+;; using a vi(m) emulator is less attractive.
+;;
+;; [boon-mode](https://github.com/jyp/boon#comparison-with-other-modal-layers-for-emacs)
+;; has a great list of different modal packages for Emacs.
+;;
+;; not-quite-modal will provide the following feature set:
+;;
+;; * Emacs based keybindings. `n' and `p' move by line, as you
+;;   probably know these shortcuts already.
+;;
+;; * Fallthrough. If you type a key that isn't a command, you will
+;;   drop into editing mode.
 
 ;;; License:
 
